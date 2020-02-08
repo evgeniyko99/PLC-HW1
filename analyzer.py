5d71d7e19514d0a84b0b307541b9e953604a15b2
evgeniyko99 <59245649+evgeniyko99@users.noreply.github.com>
2020-02-07 23:17:56 -0500

    Add files via upload

--- analyzer.py
+++ analyzer.py
@@ -4,13 +4,8 @@ sourceFile = open("sourcefile.txt", "r")
 document = sourceFile.read()                          
 
 for i in range(len(document)):
-
     if (document[i].isalnum()):                    
-
         print(document[i], end = "")               
-
     else:
-
         if (document[i - 1].isalnum()):            
-
             print("\n" + document[i])
